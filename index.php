58a5b70098b17609b665ba57dc63f6ab7a911528
Erick Montañez <codeams@gmail.com>
2016-10-11 03:11:09 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+  <head>
+
+    <meta charset='utf-8'>
+    <title>Browser cache file recovery</title>
+
+    <link rel='stylesheet' href='styles.css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Source+Code+Pro' rel='stylesheet'>
+
+  </head>
+  <body>
+
+    <header>
+      <span class='title'>Browser cache file recovery</span>
+      <a href='https://github.com/codeams/' class='author'>@codeams</a>
+    </header>
+
+    <span class='project-page-link'>
+      Find the complete instructions on the <a href=''>github project page</a>.
+    </span>
+
+    <textarea class='input-textarea' placeholder="Enter your file's cache string" spellcheck='false'></textarea>
+    <input type='button' class='recover-button' value='Recover text'>
+
+    <div class='output-container'>
+      <div class='output-window'>
+
+      </div>
+    </div>
+
+    <script src='/jquery.min.js'></script>
+    <script src='script.js'></script>
+
+  </body>
+</html>
---
+++ script.js
@@ -0,0 +1,111 @@
+
+$(function() {
+
+  var $head = $('head');
+  var $body = $('body');
+  var $inputTextarea = $('.input-textarea');
+  var $recoverButton = $('.recover-button');
+
+  var UiLocker = new function() {
+
+    var _this = this;
+
+    _this.lock = function() {
+      _this.unlock();
+      $body.append("<div class='ui-locker'></div>");
+    }
+
+    _this.unlock = function() {
+      $body.find('.ui-locker').remove();
+    }
+
+    var uiLockerStylesTag = '<style>.ui-locker{ width: 100%; height: 100%; position: fixed; z-index: 999; top: 0; left: 0; }</style>';
+    $head.append( uiLockerStylesTag );
+
+  }
+
+  var OutputWindow = new function() {
+
+    var _this = this;
+
+    _this.$container = $('.output-container');
+    _this.$window = $('.output-window');
+
+    _this.show = function() {
+      _this.$container.addClass('visible');
+    };
+
+    _this.hide = function() {
+      _this.$container.removeClass('visible');
+    };
+
+    _this.showTextareaOutput = function( textarea ) {
+
+      _this.hide();
+
+      _this.$window.find( 'textarea' ).remove();
+      _this.$window.append( textarea );
+
+      _this.show();
+
+    };
+
+    _this.$container.mouseup( function( event ) {
+
+      var isntWindowTheTarget = !$( _this.$window ).is( event.target );
+      var isntADecendantTheTarget = $( _this.$window ).has( event.target ).length === 0;
+
+      var isClickOutsideOfWindow = isntWindowTheTarget && isntADecendantTheTarget;
+
+      if ( isClickOutsideOfWindow ) _this.hide();
+
+    });
+
+  };
+
+  var callRecuperator = function( cacheString ) {
+
+    var recoverButtonOriginalValue = $recoverButton.attr('value');
+
+    $.ajax({
+
+      'type': 'POST',
+      'url': 'recuperator.php',
+      'dataType': 'html',
+
+      'data': {
+        'cacheString' : cacheString
+      },
+
+      beforeSend: function() {
+        UiLocker.lock();
+        $recoverButton.attr( 'value', 'Loading...' );
+      },
+
+      success: function( textarea ) {
+        OutputWindow.showTextareaOutput( textarea );
+      },
+
+      error: function( xhr, textStatus ) {
+        var errorDescription = 'An error has ocurred ('+ textStatus +'): ' + xhr;
+        alert( errorDescription );
+      },
+
+      complete: function() {
+        $inputTextarea.val('');
+        $recoverButton.attr( 'value', recoverButtonOriginalValue );
+        UiLocker.unlock();
+      }
+
+    });
+
+  }
+
+  $recoverButton.on( 'click', function() {
+
+    var cacheString = $inputTextarea.val();
+    callRecuperator( cacheString );
+
+  });
+
+});
